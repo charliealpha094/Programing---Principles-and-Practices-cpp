706c77c5834a718a2bbb97d37a408e6bb55d696a
charliealpha094 <carlosamaral94@gmail.com>
2023-07-29 23:52:55 +0100

    Start of chapter four

---
+++ chapterFour/expression/main.cpp
@@ -0,0 +1,16 @@
+// Done by Carlos Amaral (2023/07/29)
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    // Variable declaration
+    int length = 20;
+    int width = 40;
+
+    // Calculations
+    int area = length * width;
+
+    // Output
+    cout << "Area = " << area;
+}
