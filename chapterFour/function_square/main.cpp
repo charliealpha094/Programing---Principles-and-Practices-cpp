1778aa8b3ea98be4077a04e24dd002656f36bb68
charliealpha094 <carlosamaral94@gmail.com>
2023-08-03 23:26:24 +0100

    first function example

---
+++ chapterFour/function_square/main.cpp
@@ -0,0 +1,14 @@
+// Done by Carlos Amaral (2023/08/03)
+
+#include "std_lib_facilities.h"
+
+// Function declaration
+int square(int x) {
+    return x * x;
+}
+
+int main()
+{
+    cout << square(2) << '\n';
+    cout << square(10) << '\n';
+}
