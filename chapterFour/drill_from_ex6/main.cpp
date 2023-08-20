92cecf3b18a48e89893a41b90beb65315882c882
charliealpha094 <carlosamaral94@gmail.com>
2023-08-20 22:05:45 +0100

    correction of exercise

--- chapterFour/drill_from_ex6/main.cpp
+++ chapterFour/drill_from_ex6/main.cpp
@@ -2,26 +2,25 @@
 
 #include "std_lib_facilities.h"
 
-
 int main()
 {
     // Variable declaration
-    double smallest = 0;
-    double largest = 0;
     double num = 0;
-    double stop_char = 0;
+    auto smallest = numeric_limits<decltype(num)>::max();
+    auto largest = numeric_limits<decltype(num)>::min();
 
-    // User input and loop through it
-    cout << "Please, introduce a number ('|' to exit) \n";
-    while(cin >> num && stop_char != '|') {
-        cout << num << "\n";
-        if(num < smallest) {
+    // User input and loop through the inputted numbers
+    cout << "Please, introduce some numbers: ( | to exit)" << endl;
+
+    while(cin >> num) {
+        cout << num << endl;
+        if(num <= smallest) {
             smallest = num;
-            cout << "The smallest so far is: " << smallest << "\n";
-        } else if(num > largest) {
+            cout << "The smallest so far is: " << smallest << endl;
+        } else if(num >= largest) {
             largest = num;
-            cout << "The largest so far is: " << largest << "\n";
+            cout << "The largest so far is: " << largest << endl;
         }
     }
-    cout << "You're leaving the program!";
+    cout << "You're leaving the program! Bye!!" << endl;
 }
